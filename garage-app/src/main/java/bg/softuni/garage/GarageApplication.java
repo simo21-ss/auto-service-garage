2092ffaf393ee984682fb0416b7b28f6a12b1da3
Simeon Rosenov Simeonov <simeonsimeonov006@gmail.com>
2026-08-17 20:42:57 +0300

    chore: scaffold garage main application with Maven wrapper

---
+++ garage-app/src/main/java/bg/softuni/garage/GarageApplication.java
@@ -0,0 +1,12 @@
+package bg.softuni.garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageApplication.class, args);
+    }
+}
