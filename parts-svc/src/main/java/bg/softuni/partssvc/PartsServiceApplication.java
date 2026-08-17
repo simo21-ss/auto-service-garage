953afe136b607784a15f023e8e17eadabfb8ee42
Simeon Rosenov Simeonov <simeonsimeonov006@gmail.com>
2026-08-17 20:42:57 +0300

    chore: scaffold parts inventory microservice

---
+++ parts-svc/src/main/java/bg/softuni/partssvc/PartsServiceApplication.java
@@ -0,0 +1,12 @@
+package bg.softuni.partssvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartsServiceApplication.class, args);
+    }
+}
