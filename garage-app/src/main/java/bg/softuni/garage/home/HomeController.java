9277d5e513541b693120b5bda26815a1040cf4a7
Simeon Rosenov Simeonov <simeonsimeonov006@gmail.com>
2026-08-19 09:03:44 +0300

    feat: show live workshop statistics and role based navigation

--- garage-app/src/main/java/bg/softuni/garage/home/HomeController.java
+++ garage-app/src/main/java/bg/softuni/garage/home/HomeController.java
@@ -1,14 +1,30 @@
 package bg.softuni.garage.home;
 
+import bg.softuni.garage.mechanic.MechanicService;
+import bg.softuni.garage.repairorder.RepairOrderService;
+import bg.softuni.garage.repairorder.RepairOrderStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    private final RepairOrderService repairOrderService;
+    private final MechanicService mechanicService;
+
+    public HomeController(RepairOrderService repairOrderService, MechanicService mechanicService) {
+        this.repairOrderService = repairOrderService;
+        this.mechanicService = mechanicService;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("openOrders", repairOrderService.countOpen());
+        model.addAttribute("completedOrders",
+                repairOrderService.countByStatus(RepairOrderStatus.COMPLETED));
+        model.addAttribute("availableMechanics", mechanicService.findActive().size());
         return "index";
     }
 
