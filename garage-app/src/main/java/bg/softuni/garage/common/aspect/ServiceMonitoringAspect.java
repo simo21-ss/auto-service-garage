38e7b510e4375b5a06c5a733f2de4412949f850b
Simeon Rosenov Simeonov <simeonsimeonov006@gmail.com>
2026-08-22 09:10:07 +0300

    feat: add an aspect that times and reports service calls

---
+++ garage-app/src/main/java/bg/softuni/garage/common/aspect/ServiceMonitoringAspect.java
@@ -0,0 +1,46 @@
+package bg.softuni.garage.common.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ServiceMonitoringAspect {
+
+    private static final long SLOW_CALL_MILLIS = 500;
+
+    @Pointcut("within(@org.springframework.stereotype.Service bg.softuni.garage..*)")
+    public void serviceLayer() {
+    }
+
+    @Around("serviceLayer()")
+    public Object measure(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startedAt = System.nanoTime();
+        Object result = joinPoint.proceed();
+        long elapsedMillis = (System.nanoTime() - startedAt) / 1_000_000;
+
+        if (elapsedMillis >= SLOW_CALL_MILLIS) {
+            log.warn("{} took {} ms", signatureOf(joinPoint), elapsedMillis);
+        } else if (log.isDebugEnabled()) {
+            log.debug("{} took {} ms", signatureOf(joinPoint), elapsedMillis);
+        }
+        return result;
+    }
+
+    @AfterThrowing(pointcut = "serviceLayer()", throwing = "exception")
+    public void onFailure(JoinPoint joinPoint, Throwable exception) {
+        log.warn("{} rejected the call: {}", signatureOf(joinPoint), exception.getMessage());
+    }
+
+    private String signatureOf(JoinPoint joinPoint) {
+        return joinPoint.getSignature().getDeclaringType().getSimpleName()
+                + "." + joinPoint.getSignature().getName();
+    }
+}
